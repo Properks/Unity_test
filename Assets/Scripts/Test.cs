355cf84cad62c303be55f3954ee4b1841e5a13df
Jeongmo <wjdah1402@gmail.com>
2023-04-27 17:04:50 +0900

    How to modify scale and rotate with Quarternion, Orthogonality vector

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -15,14 +15,23 @@ public class Test : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            this.transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+            this.transform.position += this.transform.forward * Time.deltaTime;
             // this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime); // Same as above it.
+            // forward = Vector3(0, 0, 1)   up = Vector3(0, 1, 0)  right = Vector3(1, 0, 0)
 
-            rotation += new Vector3(90, 0, 0) * Time.deltaTime;
-            this.transform.eulerAngles = rotation;
-            // this.transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime); // Same as above it.
+            // rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            // this.transform.eulerAngles = rotation;
+            // this.transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime); // Same as above it. recommand it!!
             // this.transform.eulerAngles += new Vector3(90, 0 ,0) * Time.deltaTime; //FIXME: It has ERROR, when rotation is over 90.
-            Debug.Log(rotation + ", "+ transform.position);
+
+            //Other way to rotate
+            rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(rotation);
+            // Why use it? -> When any rotation is 90, other rotation has error in euler. (GIMBAL LOCK)
+
+
+            this.transform.localScale += new Vector3(2, 2, 2) * Time.deltaTime;
+            Debug.Log(transform.localScale);
         }
         // Time : Class include something relate time
         // deltatime is Δt. `new Vector3(0, 0, 1) * Time.deltaTime` mean that move 1 per 1 sec to Z axis.
