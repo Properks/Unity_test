c4254084bd6801182bcbdc8ded43a689d79b52b7
Jeongmo <wjdah1402@gmail.com>
2023-04-28 15:10:50 +0900

    Velocity method and Drag

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    Vector3 rotation;
-    [SerializeField]
-    private GameObject camera;
 
+    // [SerializeField]
+    private Rigidbody myRigid;
     // User this for initilization
     void Start() {
-        rotation = this.transform.eulerAngles;
+        myRigid = GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
     {
-        this.transform.RotateAround(camera.transform.position, Vector3.up, 90 * Time.deltaTime);
-    } // RotateAround(Around what, X or Y or Z Orthogonality, Speed)
+        if (Input.GetKey(KeyCode.W))
+        {
+            // this.transform.Translate(Vector3.forward * Time.deltaTime);
+            myRigid.velocity = Vector3.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            myRigid.angularVelocity = Vector3.up;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            myRigid.angularVelocity = Vector3.down;
+        }
+    }
 }
