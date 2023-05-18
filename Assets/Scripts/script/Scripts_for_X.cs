21e2df488b19850e8815007f72429a9f95156616
Jeongmo <wjdah1402@gmail.com>
2023-05-18 16:39:06 +0900

    Implement Jump and land with Trigger

--- Assets/Scripts/script/Scripts_for_X.cs
+++ Assets/Scripts/script/Scripts_for_X.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class Scripts_for_X : MonoBehaviour
 {
     private Rigidbody myRigid;
+    private BoxCollider col;
     [SerializeField] private GameObject field;
+    [SerializeField] private LayerMask Bottom;
     private Vector3 first_position;
 
     private Animator animator;
-    [SerializeField] private float movespeed;
-    private bool ismove;
+    [SerializeField] private float movespeed, jumpforce;
+    private bool ismove, isjump;
     // Start is called before the first frame update
     void Start() {
         myRigid = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        col = GetComponent<BoxCollider>();
         first_position = this.transform.position;
     }
 
     // Update is called once per frame
     void Update() {
+        
+        Change_Flash_according_to_move();
+
+        Jump();
+
+        Initialize_position();
+    }
+    
+    private void Change_Flash_according_to_move() {
         float dirX = Input.GetAxisRaw("Horizontal"); // If you input <-, ->, a, d, return 1. If not, return -1;
         float dirZ = Input.GetAxisRaw("Vertical"); // If you input uparrow, downarrow, w, s, return 1. If not, return -1;
         
@@ -33,11 +45,25 @@ public class Scripts_for_X : MonoBehaviour
         animator.SetBool("Move", ismove);
         animator.SetFloat("DirX", direction.x);
         animator.SetFloat("DirZ", direction.z);
+    }
 
-        // Set animation
+    private void Jump() {
+        if (isjump) {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, col.bounds.extents.y + 0.1f, Bottom)) {
+                isjump = false;
+                animator.SetTrigger("Land");
+            }
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && !isjump) {
+            isjump = true;
+            myRigid.AddForce(Vector3.up * jumpforce);
+            animator.SetTrigger("Jump");
+        }
+    }
 
-        // if it fall under field, reset position
+    private void Initialize_position() {
         if (this.transform.position.y < field.transform.position.y - this.transform.localScale.y) {
             this.transform.position = first_position;
             myRigid.velocity = Vector3.zero;
