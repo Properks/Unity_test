159cdc4cea024fd7c5bb714b30e3ff1b3f28967f
Jeongmo <wjdah1402@gmail.com>
2023-05-20 17:31:05 +0900

    Diplay cooldown with UI;

--- Assets/Scripts/script/For_UI.cs
+++ Assets/Scripts/script/For_UI.cs
@@ -7,9 +7,38 @@ public class For_UI : MonoBehaviour
 {
     [SerializeField] private Text txt_name;
     [SerializeField] private Image img_name;
+    [SerializeField] private Sprite sprite;
+
+    private bool isCoolTime = false;
+    private float currentTime = 5f;
+    private float delaytime = 5f;
+    private Color color;
+    void Start() {
+       color = img_name.color; // Inoreder to transparent 
+       color.a = 0f;
+    }
+
+    void Update() {
+        // img_name.sprite = sprite;
+        img_name.color = color;
+
+        if (isCoolTime) {
+            currentTime -= Time.deltaTime;
+            img_name.fillAmount = currentTime / delaytime; // Display cooldown
+
+   
+            if (currentTime <= 0) {
+                isCoolTime = false;
+                currentTime = delaytime;
+                color.a = 0f;
+                img_name.fillAmount = currentTime;
+            }
+        }
+    }
 
     public void Change() {
         txt_name.text = "Changed";
-        img_name.fillAmount = 0.5f;
-    }
+        isCoolTime = true;
+        color.a = 1f;
+    } 
 }
