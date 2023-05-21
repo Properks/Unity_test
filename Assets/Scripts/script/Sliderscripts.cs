66f5348d51d174358af6d7585a18ce45e7e496b0
Jeongmo <wjdah1402@gmail.com>
2023-05-21 16:19:43 +0900

    Implement DOT damage with Time and button

--- Assets/Scripts/script/Sliderscripts.cs
+++ Assets/Scripts/script/Sliderscripts.cs
@@ -6,8 +6,25 @@ using UnityEngine.UI;
 public class Sliderscripts : MonoBehaviour
 {
     [SerializeField] private Slider slider;
+    private float dot;
+    private bool isClick;
 
+    void Start() {
+        dot = 1f;
+        isClick = false;
+    }
+
+    void Update() {
+        if (isClick) {
+            dot -= Time.deltaTime;
+            slider.value -= Time.deltaTime;
+            if (dot <= 0) {
+                isClick = false;
+                dot = 1f;
+            }
+        }
+    }
     public void button() {
-        slider.value -= 0.1f;
+        isClick = true;
     }
 }
